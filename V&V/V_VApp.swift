33adf8a5b697fe17ed960d081f9f33d281c802a6
Atilla Köz <115349531+Atilla-Koz@users.noreply.github.com>
2024-12-27 01:57:10 +0300

    Initial Commit

---
+++ V&V/V_VApp.swift
@@ -0,0 +1,17 @@
+//
+//  V_VApp.swift
+//  V&V
+//
+//  Created by Atilla Köz on 27.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct V_VApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ V&VTests/V_VTests.swift
@@ -0,0 +1,17 @@
+//
+//  V_VTests.swift
+//  V&VTests
+//
+//  Created by Atilla Köz on 27.12.2024.
+//
+
+import Testing
+@testable import V_V
+
+struct V_VTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
